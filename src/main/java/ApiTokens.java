c95234547116a254dac752d28e678fdeeb79ca03
fataler <fataler69@yandex.ru>
2017-03-12 04:09:50 +0300

    Initial Commit

---
+++ src/main/java/ApiTokens.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Fataler on 12.03.2017.
+ */
+public class ApiTokens {
+    private static String tokenBot="MjkwMjI1NDUzMzU0OTc1MjMy.C6X90g.A_237bqcbTOOK50Gb8Ovy35htC0";
+    private static String tokenRiot="0b39e1a8-19f1-4714-ab1c-58b831f5c28b";
+
+    public static String getBotToken(){
+        return tokenBot;
+    }
+    public static String getRiotToken(){
+        return tokenRiot;
+    }
+
+}
+
+
+
