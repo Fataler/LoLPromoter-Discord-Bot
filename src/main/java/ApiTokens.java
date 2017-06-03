03aafff1a02045ef4a0f7661a354f2f70ef90977
fataler <fataler69@yandex.ru>
2017-06-03 10:59:02 +0300

    Revert "Refactoring"
    This reverts commit b5cca75d47235ce5dd72cf4a66f492173af8880b.

--- src/main/java/ApiTokens.java
+++ src/main/java/ApiTokens.java
@@ -3,7 +3,7 @@
  */
 public class ApiTokens {
     private static String tokenBot="MjkwMjI1NDUzMzU0OTc1MjMy.C6X90g.A_237bqcbTOOK50Gb8Ovy35htC0";
-    private static String tokenRiot="RGAPI-211e4553-315b-4580-94cb-584034992a15";
+    private static String tokenRiot="0b39e1a8-19f1-4714-ab1c-58b831f5c28b";
 
     public static String getBotToken(){
         return tokenBot;
